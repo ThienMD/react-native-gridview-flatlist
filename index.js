15f681a67ac2f3f1a7de7fa8f5fd2378d6d61a94
Van Thien <pvthien1993@gmail.com>
2017-07-28 11:20:12 +0700

    fix bug

--- index.js
+++ index.js
@@ -10,50 +10,77 @@ import {
 } from 'react-native';
 
 var CollectionView = React.createClass({
-    groupItems: function(items, itemsPerRow) {
-        var itemsGroups = [];
-        var group = [];
-        items.forEach(function(item) {
-          if (group.length === itemsPerRow) {
-            itemsGroups.push(group);
-            group = [item];
-          } else {
-            group.push(item);
-          }
-        });
+  groupItems: function (items, itemsPerRow) {
+    var itemsGroups = [];
+    var group = [];
+    items.forEach(function (item) {
+      if (group.length === itemsPerRow) {
+        itemsGroups.push(group);
+        group = [item];
+      } else {
+        group.push(item);
+      }
+    });
 
-        if (group.length > 0) {
-          itemsGroups.push(group);
+    if (group.length > 0) {
+      itemsGroups.push(group);
+    }
+
+    return itemsGroups;
+  },
+  renderGroup: function ({ item }) {
+    var that = this;
+    var items = item.map(function (obj, index) {
+      return that.props.renderItem(obj, index);
+    });
+    items = items.map((obj, i) => {
+      return (
+        <View style={{ flex: 1 }}>
+          {obj}
+        </View>)
+
+    })
+    return (
+      <View style={styles.group}>
+        {
+          items
+        }
+        {this.getBlankCell(item.length, this.props.itemsPerRow)
         }
 
-        return itemsGroups;
-    },
-    renderGroup: function({item}) {
-      var that = this;
-      var items = item.map(function(obj, index) {
-        return that.props.renderItem(obj, index);
-      });
-      return (
-        <View style={styles.group}>
-          {items}
-        </View>
-      );
-    },
-    render: function() {
-        var groups = this.groupItems(this.props.items, this.props.itemsPerRow);
-        return (<FlatList
-         data={groups}
-          {...this.props}
-          renderItem={this.renderGroup}
-        />);
-    },
+      </View>
+    );
+  },
+
+  getBlankCell(currentItems, itemsPerRow) {
+
+    if (currentItems < itemsPerRow) {
+      var arr = []
+      for (var i = 0; i < itemsPerRow - currentItems; i++) {
+        arr.push(
+          <View style={{ flex: 1 }}>
+          </View>
+        )
+      }
+      return arr
+    }
+
+  },
+  render: function () {
+    var groups = this.groupItems(this.props.items, this.props.itemsPerRow);
+    return (<FlatList
+      data={groups}
+      {...this.props}
+      renderItem={this.renderGroup}
+    />);
+  },
 });
 
 
 var styles = StyleSheet.create({
   group: {
     flexDirection: 'row',
-    alignItems: 'center',
+    alignItems: 'flex-start',
     justifyContent: 'center',
     overflow: 'hidden'
   }
